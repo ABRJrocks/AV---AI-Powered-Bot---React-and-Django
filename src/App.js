2ca251401bbd41d8aebf8d8e4c369dd7b6909726
ABRJrocks <40339959+ABRJrocks@users.noreply.github.com>
2023-09-11 19:44:12 +0500

    Init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Form from "./components/FormSignin";
+import MainPage from "./components/mainPage"
+function App() {
+  return (
+    <div className="App">
+      <MainPage></MainPage>
+    </div>
+  );
+}
+export default App;
